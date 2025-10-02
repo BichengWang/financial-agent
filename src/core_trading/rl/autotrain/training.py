81cf5954ee5d452b0430b57b0d84b580f4cd88fa
Minchen Wang <minchenwang@outlook.com>
2025-10-02 01:02:43 -0700

    Resolve package import

--- src/core_trading/rl/autotrain/training.py
+++ src/core_trading/rl/autotrain/training.py
@@ -11,7 +11,7 @@ from ..config import config
 from ..marketdata.yahoodownloader import YahooDownloader
 from ..preprocessing.preprocessors import FeatureEngineer
 from ..preprocessing.data import data_split
-from ..env.env_stocktrading import StockTradingEnv
+from ..environment.env_stocktrading import StockTradingEnv
 from ..model.models import DRLAgent
 from ..trade.backtest import backtest_stats, backtest_plot, get_daily_return, get_baseline
 
